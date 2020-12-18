ec430e85a6ceacde0c923efedfcd646fd0a34f1b
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-12-18 10:10:15 +0100

    Création du serveur

---
+++ server.js
@@ -0,0 +1,40 @@
+  
+/* 
+Imports
+*/
+    // NPM modules
+    require('dotenv').config(); //=> https://www.npmjs.com/package/dotenv
+    const express = require('express'); //=> https://www.npmjs.com/package/express
+    const bodyParser = require('body-parser'); //=> https://www.npmjs.com/package/body-parser
+    const cookieParser = require('cookie-parser'); //=> https://www.npmjs.com/package/cookie-parser
+    const ejs = require('ejs'); //=> https://www.npmjs.com/package/ejs
+    const passport = require('passport'); //=> https://www.npmjs.com/package/passport
+    const path = require('path'); //=> https://www.npmjs.com/package/path
+//
+
+/* 
+Server class
+*/
+class ServerClass{
+    constructor(){}
+
+    init(){
+
+    }
+
+    config(){
+
+    }
+
+    launch(){
+
+    }
+}
+//
+
+/* 
+Start server
+*/
+    const NODEapi_boilerplate = new ServerClass();
+    NODEapi_boilerplate.init();
+//
