a218968d5e99a468bdc0abb5a16163dff98c21f4
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-12-18 12:34:06 +0100

    facto du controller iris

--- controllers/iris.controller.js
+++ controllers/iris.controller.js
@@ -9,10 +9,28 @@ Functions
 */
     // CRUD: create object
     const createObject = (req) => {
+        // Convert species for training
+        let species = null;
+
+        if( req.body.Species === 'Iris-setosa' ){ species = [1,0,0] }
+        else if( req.body.Species === 'Iris-versicolor' ){ species = [0,1,0] }
+        else if( req.body.Species === 'Iris-virginica' ){ species = [0,0,1] }
+        
+        // Define object to create
+        // +var => parseInt(var) || parseFloat(var)
+        const newObject = {
+            SepalLengthCm: +req.body.SepalLengthCm,
+            SepalWidthCm: +req.body.SepalWidthCm,
+            PetalLengthCm: +req.body.PetalLengthCm,
+            PetalWidthCm: +req.body.PetalWidthCm,
+            Species: species,
+            SpeciesName: req.body.Species
+        }
+
         // Return new Promise
         return new Promise( (resolve, reject) => {
             // Use the Mongoose model to create data
-            Models.iris.create(req.body)
+            Models.iris.create(newObject)
             .then( data => resolve(data) )
             .catch( err => reject(err) )
         })
--- routers/backend.router.js
+++ routers/backend.router.js
@@ -25,27 +25,6 @@ Routes definition
 
             // Define iris POST route
             this.router.post('/iris', (req, res) => {
-                // Convert species for training
-                let species = null;
-
-                if( req.body.Species === 'Iris-setosa' ){ species = [1,0,0] }
-                else if( req.body.Species === 'Iris-versicolor' ){ species = [0,1,0] }
-                else if( req.body.Species === 'Iris-virginica' ){ species = [0,0,1] }
-                
-                // Define object to create
-                // +var => parseInt(var) || parseFloat(var)
-                const newObject = {
-                    SepalLengthCm: +req.body.SepalLengthCm,
-                    SepalWidthCm: +req.body.SepalWidthCm,
-                    PetalLengthCm: +req.body.PetalLengthCm,
-                    PetalWidthCm: +req.body.PetalWidthCm,
-                    Species: species,
-                    SpeciesName: req.body.Species
-                }
-                
-                // Change request body 
-                req.body = newObject;
-
                 // Use the controller to create new object
                 Controllers.iris.createObject(req)
                 .then( data => res.redirect('/'))
